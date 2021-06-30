0af7735b786b0c023a04da4b80f010988d7eb154
shivonaaa <84503998+shivonaaa@users.noreply.github.com>
2021-07-01 00:51:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 include_once("shivonaferns.html")
  include_once('database.php')
 
-  if(isset($_POST['submitbtn']))
-{	 
-	 $email = $_POST['email'];
-	 $password = $_POST['password'];
-	  //$query1 = "create table if not exists user (email varchar(225),password varchar(255))";
-	 $query = "INSERT INTO user(email,password) values ('$email','$password')";
-	 if($result = pg_query($query)){
-		echo "Data Added Successfully.";
-	 }
-	 else{
-		echo "Error.";
-	 }
-}
+//   if(isset($_POST['submitbtn']))
+// {	 
+// 	 $email = $_POST['email'];
+// 	 $password = $_POST['password'];
+// 	  //$query1 = "create table if not exists user (email varchar(225),password varchar(255))";
+// 	 $query = "INSERT INTO user(email,password) values ('$email','$password')";
+// 	 if($result = pg_query($query)){
+// 		echo "Data Added Successfully.";
+// 	 }
+// 	 else{
+// 		echo "Error.";
+// 	 }
+// }
   
 ?>
