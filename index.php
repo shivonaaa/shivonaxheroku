0d33ec1697e191d18d9b75337a7fd15c04862ae9
shivonaaa <84503998+shivonaaa@users.noreply.github.com>
2021-06-30 23:58:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
 include_once("shivonaferns.html")
+  include_once 'database.php';
 
+  if(isset($_POST['submitbtn']))
+{	 
+	 $email = $_POST['email'];
+	 $password = $_POST['password'];
+	 $query = "INSERT INTO user(email,password) values ('$email','$password')";
+	 if($result = pg_query($query)){
+		echo "Data Added Successfully.";
+	 }
+	 else{
+		echo "Error.";
+	 }
+}
+  
 ?>
