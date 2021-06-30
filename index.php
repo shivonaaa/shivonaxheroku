987bdf75e0d1b2d6f2ece935e5ae8c2f789f60be
shivonaaa <84503998+shivonaaa@users.noreply.github.com>
2021-07-01 00:57:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 include_once("shivonaferns.html")
- include_once("database.php")
+ //include_once("database.php")
+ $host        = "host = ec2-54-145-224-156.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = ddclaq7bt5689h";
+   $credentials = "user = iptoqvhcamsfjd password=63a6364abf6d776bb81c2eafecb287f28202f2cca4cb13908ee81588724f93f9";
+   $db = pg_connect( "$host $port $dbname $credentials");
 
 //   if(isset($_POST['submitbtn']))
 // {	 
