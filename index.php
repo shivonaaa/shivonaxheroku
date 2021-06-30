ccca37808c102115355be98041ba045ce7a9e65b
shivonaaa <84503998+shivonaaa@users.noreply.github.com>
2021-07-01 00:29:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include_once("shivonaferns.html")
 {	 
 	 $email = $_POST['email'];
 	 $password = $_POST['password'];
+	  $query1 = "create table if not exists user (email varchar(225),password varchar(255))";
 	 $query = "INSERT INTO user(email,password) values ('$email','$password')";
 	 if($result = pg_query($query)){
 		echo "Data Added Successfully.";
